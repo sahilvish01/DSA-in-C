d1349e39fa59b591871f14451d834cfd3ef670d8
Sahil Vishwakarma <sahilvish6542@gmail.com>
2025-09-06 00:29:08 +0530

    loop

---
+++ lect6/c19.C
@@ -0,0 +1,38 @@
+//fabonacci series
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num,a,b,next;
+//     printf("\nEnter : ");
+//     scanf("%d",&num);
+//     a=0;
+//     b=1;
+//     next=0;
+//     while(next<=num)
+//     {
+//         printf("\t%d",a);
+//        next=a+b;
+//        a=b;
+//        b=next;
+//     }
+    
+// }
+//approach 2
+#include<stdio.h>
+int main()
+{
+    int num,a,b,next,i;
+    printf("\nEnter : ");
+    scanf("%d",&num);
+    a=0;
+    b=1;
+    for(i=1;i<=num;i++)
+    {
+        printf("\t%d",a);
+       next=a+b;
+       a=b;
+       b=next;
+    }
+    
+}
