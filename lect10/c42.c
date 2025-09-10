e7801aa82d51b3d85aa0b31594873012c73494c0
sahilvish01 <sahilvish6542@gmail.com>
2025-09-10 14:09:09 +0530

    code

---
+++ lect10/c42.c
@@ -0,0 +1,30 @@
+// Q)  *** ***
+//     **   **
+//     *     *
+
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<3;i++)
+    {
+        
+        for(int j=2;j>=i;j--)
+        {
+            printf("*");
+        }
+        for(int j=0;j<i;j++)
+        {
+            printf(" ");
+        }
+        
+        for(int j=0;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=2;j>=i;j--)
+        {
+            printf("*");
+        }
+        printf("\n");      
+    }
+}
